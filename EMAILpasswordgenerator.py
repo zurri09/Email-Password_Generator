dff09bbc2038f692f966c4f5edbfe35eb8830170
zurri09 <54475439+zurri09@users.noreply.github.com>
2019-08-24 20:03:32 +0200

    Add files via upload

---
+++ EMAILpasswordgenerator.py
@@ -0,0 +1,23 @@
+import random
+import string
+
+print("How many random email & password combos would you like?")
+
+amount = int(input("Insert amount here:"))
+
+for i in range(amount):
+    names = open("names_and_things.txt").read().splitlines()
+
+    random_name = random.choice(names).strip()
+
+    numbers = random.randint(49, 293238)
+
+    email = random_name + str(numbers) + "@gmail.com"
+
+    characters = string.ascii_letters + string.digits + "$@#%&^*()"
+
+    password = "".join(random.choice(characters)for i in range(15))
+
+    print(email,password)
+
+input("PRESS ENTER TO EXIT")
